580fb53fa222d61cc468bc11559482e2720b9b90
meow530 <38370386+meow530@users.noreply.github.com>
2020-02-26 17:52:54 -0500

    Update ui.r

--- app/ui.r
+++ app/ui.r
@@ -1,39 +1,62 @@
 library(shiny)
 library(shinydashboard)
+library(devtools)
+#install_github("nik01010/dashboardthemes")
+library(dashboardthemes)
 library(leaflet)
-library(dplyr)
-library(DT)
 library(leaflet.extras)
+library(DT)
 library(rsconnect)
+library (png)
+
 
 ## =========== Dashboard Header ===========
-header <- dashboardHeader(title = 'NYC Restaurant Inspection')
+header <- dashboardHeader(title = 'NYC Restaurant Inspection',
+                          titleWidth = 265)
+
 
 ## =========== Dashboard Sidebar ===========
-sidebar <- dashboardSidebar(sidebarMenu(
+sidebar <- dashboardSidebar(
+  width = 265,
+  
+  # tags$head(tags$style(HTML('.logo {
+  #                             background-color: #8565c4 !important;
+  #                           }
+  #                           .navbar {
+  #                           background-color: #8565c4 !important;
+  #                           }
+  #                           '))),
+  # uiOutput("userpanel"),
+  
+  sidebarMenu(
   menuItem("Home", tabName = "Home", icon = icon("home")),
   menuItem("Introduction", tabName = "Introduction", icon = icon("clipboard"),
            menuSubItem("Guide", tabName = "Guide", icon = icon("arrow-alt-circle-right")),
-           menuSubItem("Data Overview", tabName = "Source", icon = icon("arrow-alt-circle-right")),
-           menuSubItem("About Team", tabName = "Team", icon = icon("arrow-alt-circle-right"))),
+           menuSubItem("Data Overview", tabName = "Source", icon = icon("arrow-alt-circle-right"))),
   menuItem("Map", tabName = "Map", icon = icon("map")),
-  menuItem("Summary Statistics", tabName = "Statistics", icon = icon("pie-chart")))
+ # menuItem("Heatmap", tabName = "Heatmap", icon = icon("map")),
+  menuItem("Summary Statistics", tabName = "Statistics", icon = icon("pie-chart")),
+  menuItem("About Team", tabName = "Team", icon = icon("address-card")))
 )
+
 
 ## =========== Dashboard Body ===========
 ## Home
+
+
 body.home <- tabItem(tabName = "Home",
-                         fluidPage(
-                           fluidRow(
-                             box(width = 15, title = "Home",
-                                 solidHeader = TRUE, 
-                                 h4("NYC Restaurant Health Inspection"),
-                                 h5("Eating in New York can be very enjoyable, but there are also potential health risks. With the huge number of restaurants in New York, supervising the restaurants became a tough task. Our shiny app is about the inspection results of restaurants in the five boroughs. The data comes from the Health Department."),
-                                 h5("Our target consumers are mainly the restaurant supervisors, who’s job duties are maintaining the overall quality control and ensure customer satisfaction. Using our app, they can explore different types of violations, where are they concentrated and what cuisines are highly potential to have certain violations, etc. According to this they can choose where they are interested to work and help getting better."),  
-                                 h5("Individuals like residents or travelers are also welcomed to use the app. They can get some references when choosing the next place to eat and enjoy.")
-                                 )
-                             )
-                     ))
+                     fluidPage(
+                       fluidRow(
+                         column(3, valueBox("Area", 'New York City', icon("home"), color = 'purple', width = 15)),
+                         column(3, valueBox("Data", "86004 Restaurants", icon("hamburger"), color = 'purple', width = 15)),
+                         column(3, valueBox("Goal", "Restaurant Violation Inspection", icon("table"), color = 'purple', width = 20))),
+                       
+                       fluidRow(column(3, img(src = "home_page.png", height = 500, width = 800)))
+                       )
+                     )
+
+
+
 ## Introduction - Guide
 body.guide <- tabItem(tabName = "Guide",
                       fluidPage(
@@ -60,34 +83,66 @@ body.source <- tabItem(tabName = "Source",
 body.team <- tabItem(tabName = "Team",
                      mainPanel(
                        h3(textOutput("team0")),
-                       h5(textOutput("team1"))
+                       h5(textOutput("team1")),
+                       h5(textOutput("team2")),
+                       h5(textOutput("team3")),
+                       h5(textOutput("team4")),
+                       h5(textOutput("team5")),
+                       h5(textOutput("team6"))
                      ))
 
 ## Map
 body.map <- tabItem(tabName = "Map",
                     fluidPage(
-                      fluidRow(column(4, 
+                      fluidRow(column(3, 
                                       selectizeInput("boro1", "Choose the Borough",
                                                      choices = c("Choose Boro(s)" = "",
                                                                  "Bronx", "Brooklyn",
                                                                  "Manhattan", "Queens",
                                                                  "Staten Island"),
-                                                     selected = c("Manhattan"),
+                                                     #selected = c("Manhattan"),
                                                      multiple = T)),
-                               column(4, 
+                               column(3, 
                                       selectizeInput("grade1", "Choose the Grade",
                                                      choices = c("Choose Grade(s)" = "",
                                                                  "A", "B", "C",
                                                                  "P", "N", "Z"),
                                                      selected = c("A"),
                                                      multiple = T)),
-                               column(4, 
+                               column(3, 
                                       selectizeInput("cuisine1", "Cuisine Selection:",
                                               choices = c("American", "Chinese", "Mexican", "Italian", "Japanese", "Caribbean", "Spanish"),
                                               selected = c("American"), multiple = T))),
                       fluidRow(column(12, leafletOutput("mapMarker"))
                       )
                     ))
+
+## Heatmap
+body.heatmap <- tabPanel(tabName = "Heatmap",
+
+         sidebarLayout(
+
+           mainPanel(
+             leafletOutput("heatMap",width="100%",height=700)),
+
+           sidebarPanel(
+             # Input: Slider for time of the day ----
+             sliderInput(inputId = "score",
+                         label = "Select score:",
+                         min = 0,
+                         max = 100,
+                         value = 50)
+           )
+         )
+         # fluidPage(
+         #   fluidRow(leafletOutput("mapAct", height = "800px")),
+         #   fluidRow(absolutePanel(top = 150, right = 20,
+         #                          sliderInput("animation", "Time(30 minutes)", min = 0, 
+         #                                      max = 47, value = 0, step = 1, 
+         #                                      animate = animationOptions(interval = 500, loop = FALSE)))),
+         #   fluidRow(absolutePanel(plotOutput("line"), top = 80, left = 300,
+         #                          width = 360, height = 300, draggable = TRUE)))),
+)
 
 ## Statistics
 body.statistics <- tabItem(tabName = "Statistics",
@@ -114,33 +169,59 @@ body.statistics <- tabItem(tabName = "Statistics",
                                                          choices = c("American", "Chinese", "Mexican", "Italian", "Japanese", "Caribbean", "Spanish"))),
                                              (plotlyOutput("donut", height = 400))),
                                     
-                                    tabPanel(title = "Violation Score",
-                                             width = 12,
-                                             plotOutput("barplot3", height = 500)),
+                                    # tabPanel(title = "Violation Score",
+                                    #          width = 12,
+                                    #          plotOutput("barplot3", height = 500)),
+                                    
+                                    # tabPanel(title = "Total Violation Score", width = 12,
+                                    #          plotlyOutput("barplot4", height = 500)),
+                                    
+                                    tabPanel(title = "Average Violation Score", width = 12,
+                                             plotlyOutput("barplot5", height = 500)),
                                     
                                     tabPanel(title = "Year Changes",
                                              width = 12,
-                                             plotOutput("barplot4", height = 500))
+                                             plotOutput("barplot6", height = 500))
                              )
                            ))
 
+body.team <- tabItem(tabName = "Team",
+                     mainPanel(
+                       h3(textOutput("team0")),
+                       h5(textOutput("team1")),
+                       h5(textOutput("team2")),
+                       h5(textOutput("team3")),
+                       h5(textOutput("team4")),
+                       h5(textOutput("team5")),
+                       h5(textOutput("team6"))
+                     ))
+
+
 body <- dashboardBody(
+  #changing theme
+  shinyDashboardThemes(
+    theme = "purple_gradient"),
+  
+  # UI tabs
   tabItems(
     body.home,
     body.guide,
     body.source,
-    body.team,
     body.map,
-    body.statistics
+    #body.heatmap,
+    body.statistics,
+    body.team
   )
 )
 
 
 ## =========== UI Interface ===========
 ui <- dashboardPage(
-  skin = "purple",
+  #skin = "purple",
   header,
   sidebar,
   body
 )
+
+
 
